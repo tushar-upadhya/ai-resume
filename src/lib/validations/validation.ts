58085564b28597dced7f0f484912c8aa9a9d6292
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2024-12-26 10:22:40 +0530

    ADD : color-picker-comp

--- src/lib/validations/validation.ts
+++ src/lib/validations/validation.ts
@@ -89,6 +89,7 @@ export const resumeSchema = z.object({
   ...skillsSchema.shape,
   ...summarySchema.shape,
   colorHex: optionalString,
+  borderStyle: optionalString,
 });
 
 export type ResumeValues = Omit<z.infer<typeof resumeSchema>, "photo"> & {
